eb6dd4ca4c7328c8140332100c0e3f074c4d60d1
Guido Dizioli <guidodizi@gmail.com>
2018-03-29 12:29:03 -0300

    url facebook update

--- config/auth.js
+++ config/auth.js
@@ -3,7 +3,7 @@ module.exports = {
   facebookAuth: {
     clientID: "895792863919984", // your App ID
     clientSecret: "a04a7056ff398083f2d80996965bc650", // your App Secret
-    callbackURL: "http://localhost:5000/auth/facebook/callback",
+    callbackURL: "https://cookat.herokuapp.com/auth/facebook/callback",
     profileURL:
       "https://graph.facebook.com/v2.5/me?fields=first_name,last_name,email",
     profileFields: ["id", "email", "name"] // For requesting permissions from Facebook API
