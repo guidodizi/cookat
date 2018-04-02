fdc6bd4630d3ee20e0ad5dc0cd2e28b57702ae2b
Guido Dizioli <guidodizi@gmail.com>
2018-04-02 20:22:45 -0300

    facebook permissions

--- config/auth.js
+++ config/auth.js
@@ -4,8 +4,9 @@ module.exports = {
     clientID: "895792863919984", // your App ID
     clientSecret: "a04a7056ff398083f2d80996965bc650", // your App Secret
     callbackURL: "https://cookat.herokuapp.com/auth/facebook/callback",
-    profileURL: "https://graph.facebook.com/v2.12/me?fields=id,name,email",
-    profileFields: ["id", "email", "displayName"] // For requesting permissions from Facebook API
+    profileURL:
+      "https://graph.facebook.com/v2.5/me?fields=first_name,last_name,email",
+    profileFields: ["id", "emails", "displayName"] // For requesting permissions from Facebook API
   },
 
   googleAuth: {
